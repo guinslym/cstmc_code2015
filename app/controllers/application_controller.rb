6f645197858073af3ef6ace1c47eb94f7732f4f8
Guinsly Mondesir <guinslym @gmail.com>
2015-02-22 10:11:52 -0500

    finish translating first page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     before_action :set_locale
  
- def default_url_options(options={})
-  { locale: I18n.locale }
-end
-
 
  private
 def set_locale
