bf6b97cebf8cd70f9e1a054f7dcb24838829d7af
Guinsly Mondesir <guinslym @gmail.com>
2015-02-22 10:14:20 -0500

    finish translation first page http://localhost:3000/?locale=fr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     before_action :set_locale
  
+ def default_url_options(options={})
+  { locale: I18n.locale }
+end
+
 
  private
 def set_locale
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  #http://localhost:3000/?locale=fr
   root 'welcome#index'
 
   # Example of regular route:
